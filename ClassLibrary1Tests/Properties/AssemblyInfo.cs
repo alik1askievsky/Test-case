7b111bbcc487bc6df2d4d956a53fbc6a9d40ff7c
gumbit <gumbit@KRIPER>
2025-04-05 16:34:18 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("ClassLibrary1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("24fe592b-b126-41fb-97f3-84e11c181048")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test-caseTests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [TestCase(1, 2, 3)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-5, 3, -2)]
+        [TestCase(100, 200, 300)]
+        public void Add_TwoNumbers_ReturnsCorrectSum(int a, int b, int expected)
+        {
+            var calculator = new Calculator();
+            int result = calculator.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
